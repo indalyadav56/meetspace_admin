d469ae19791ac014498b6b581a5da442a9f92d2d
indal <indalkumar56@gmail.com>
2024-02-09 09:09:07 +0530

    wip

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -8,43 +8,23 @@ import (
 )
 
 func GetAllUsers(ctx *fiber.Ctx) error{
-	var users []models.User
-	config.DB.Find(&users)
-	ctx.Status(200).JSON(users)
 	return nil
 }
 
 
 func DeleteUser(ctx *fiber.Ctx) error{
-	userId := ctx.Params("userId")
-	
-	var user models.User
-	config.DB.Where("id=?", userId).Delete(&user)
-	
-	ctx.Status(200).JSON(user)
 	return nil
 	
 }
-
 
 func GetAllClients(ctx *fiber.Ctx) error {
 	var clients []models.Client
 	config.DB.Find(&clients)
 	ctx.JSON(clients)
 	return nil
-	
 }
 
-
 func DeleteClient(ctx *fiber.Ctx) error{
-	clientId := ctx.Params("clientId")
-	
-	var client models.Client
-	config.DB.Where("id=?", clientId).Delete(&client)
-
-	var user models.User
-	config.DB.Where("client_id=?", clientId).Delete(&user)
-	
 	ctx.JSON("awerawer")
 	return nil
 	
